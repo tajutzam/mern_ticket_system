f5f5d019e795cf217aec63e5c273e773e80095d5
zamzami <zamzami@mitraintek.co.id>
2026-05-23 15:54:08 +0700

    feat: fna'

--- frontend/src/hooks/api/useTicketMutations.ts
+++ frontend/src/hooks/api/useTicketMutations.ts
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { api } from "@/api/axiosInstance"; // Diperlukan untuk fetch data user teknisi & buat assignment
+import { api } from "@/api/axiosInstance"; 
 import {
   fetchAllTickets,
   fetchTicketById,
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -73,14 +73,14 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { title: "Lovable App" },
-      { name: "description", content: "Lovable Generated Project" },
-      { name: "author", content: "Lovable" },
-      { property: "og:title", content: "Lovable App" },
-      { property: "og:description", content: "Lovable Generated Project" },
+      { title: "Sipaten" },
+      { name: "description", content: "Sipaten" },
+      { name: "author", content: "Sipaten" },
+      { property: "og:title", content: "Sipaten App" },
+      { property: "og:description", content: "Sipaten" },
       { property: "og:type", content: "website" },
       { name: "twitter:card", content: "summary" },
-      { name: "twitter:site", content: "@Lovable" },
+      { name: "twitter:site", content: "@Sipaten" },
     ],
     links: [
       {
